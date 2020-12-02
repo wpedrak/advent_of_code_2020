db53874bee516198e2d5a2a27b68928636250029
Wojciech Pędrak <wojciech@eloomi.com>
2020-12-02 07:41:22 +0100

    style of 2

--- 02/solution.py
+++ 02/solution.py
@@ -5,9 +5,10 @@ def get_lines():
     lines = [line.rstrip('\n') for line in file]
     return lines
 
-number_of_correct_passwords = 0
+def get_parsed_lines():
+    return [parse_line(line) for line in get_lines()]
 
-for line in get_lines():
+def parse_line(line):
     splitted = line.split()
     policy = splitted[0].split('-')
     from_policy = int(policy[0])
@@ -15,8 +16,12 @@ for line in get_lines():
     letter = splitted[1][:-1]
     password = splitted[2]
 
-    counter = Counter(password)
+    return from_policy, to_policy, letter, password
 
+number_of_correct_passwords = 0
+
+for from_policy, to_policy, letter, password in get_parsed_lines():
+    counter = Counter(password)
     number_of_correct_passwords += from_policy <= counter[letter] <= to_policy
 
 print(number_of_correct_passwords)
--- 02/solution2.py
+++ 02/solution2.py
@@ -1,13 +1,12 @@
-from collections import Counter
-
 def get_lines():
     file = open("input.txt", "r")
     lines = [line.rstrip('\n') for line in file]
     return lines
 
-number_of_correct_passwords = 0
+def get_parsed_lines():
+    return[parse_line(line) for line in get_lines()]
 
-for line in get_lines():
+def parse_line(line):
     splitted = line.split()
     policy = splitted[0].split('-')
     first_idx = int(policy[0])
@@ -15,8 +14,12 @@ for line in get_lines():
     letter = splitted[1][:-1]
     password = splitted[2]
 
-    number_of_matches = (password[first_idx - 1] == letter) + (password[second_idx - 1] == letter)
+    return first_idx, second_idx, letter, password
 
+number_of_correct_passwords = 0
+
+for first_idx, second_idx, letter, password in  get_parsed_lines():
+    number_of_matches = (password[first_idx - 1] == letter) + (password[second_idx - 1] == letter)
     number_of_correct_passwords += number_of_matches == 1
 
 print(number_of_correct_passwords)
