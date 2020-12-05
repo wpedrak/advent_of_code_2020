c76a941b075631a15f322afa30305704b9d55afa
Wojciech Pędrak <wojciech@eloomi.com>
2020-12-05 09:33:32 +0100

    05 done

---
+++ 05/solution.py
@@ -0,0 +1,15 @@
+def get_lines():
+    file = open("input.txt", "r")
+    lines = [line.rstrip('\n') for line in file]
+    return lines
+
+def get_id(seat: str):
+    seat = seat.replace('F', '0')
+    seat = seat.replace('B', '1')
+    seat = seat.replace('L', '0')
+    seat = seat.replace('R', '1')
+
+    return int(seat, base=2)
+
+max_id_seat = max(get_lines(), key=get_id)
+print(get_id(max_id_seat))
---
+++ 05/solution2.py
@@ -0,0 +1,23 @@
+def get_lines():
+    file = open("input.txt", "r")
+    lines = [line.rstrip('\n') for line in file]
+    return lines
+
+def get_id(seat: str):
+    seat = seat.replace('F', '0')
+    seat = seat.replace('B', '1')
+    seat = seat.replace('L', '0')
+    seat = seat.replace('R', '1')
+
+    return int(seat, base=2)
+
+all_free_ids = set(range(2**10)) - set(map(get_id, get_lines()))
+
+for free_id in all_free_ids:
+    if free_id == 0 or free_id == 2**10 -1:
+        continue
+
+    if (free_id - 1) in all_free_ids or (free_id + 1) in all_free_ids:
+        continue
+
+    print(free_id)
