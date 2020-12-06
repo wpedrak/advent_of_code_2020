0c290bfe899fbe0e260184f56b7a8049ebd98ed6
Wojciech Pędrak <wojciech@eloomi.com>
2020-12-06 09:50:43 +0100

    6 done

---
+++ 06/solution2.py
@@ -0,0 +1,20 @@
+def get_groups():
+    file = open("input.txt", "r").read()[:-1]
+    return file.split('\n\n')
+
+def get_lines_from_group(group):
+    return group.split('\n')
+
+def get_groups_lines():
+    return [get_lines_from_group(group) for group in get_groups()]
+
+sum_count = 0
+
+for group_lines in get_groups_lines():
+    group_letters = set('qwertyuioplkjhgfdsazxcvbnm')
+    for line in group_lines:
+        group_letters &= set(line)
+
+    sum_count += len(group_letters)
+
+print(sum_count)
